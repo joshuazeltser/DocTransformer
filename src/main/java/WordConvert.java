419f017bc0bed77e81b59cb6c32e98e77a82f9d1
Joshua Zeltser <joshua.zeltser@gmail.com>
2019-09-11 21:40:08 +0100

    extended abstract class to word converter

--- src/main/java/WordConvert.java
+++ src/main/java/WordConvert.java
@@ -1,2 +1,24 @@
-public class WordConvert {
+import java.io.IOException;
+
+public class WordConvert extends Converter {
+
+    public WordConvert(ConvertFile file, String outputPath) {
+        super(file, outputPath);
+    }
+
+    public boolean convertToHTML() throws IOException {
+        return false;
+    }
+
+    public boolean convertToDocx(String outputName) {
+        return false;
+    }
+
+    public boolean convertToJpeg(String outputName) {
+        return false;
+    }
+
+    public boolean convertToText(String outputName) {
+        return false;
+    }
 }
